99e566f4dd38af5b4a3f7e81a845383f0eeb75d5
Arabhaya <r.arabhaya@gmail.com>
2024-10-21 21:33:45 +0530

    ec2 branch deployment testing

--- pages/Pooja/index.jsx
+++ pages/Pooja/index.jsx
@@ -2,20 +2,19 @@ import React from "react";
 import Tabs from "../components/tab";
 
 const Pooja = () => {
-
-return(
+  return (
     <Tabs>
-  <div key="info" title="Customer info">
-    <p>We are showing customer info here</p>
-  </div>
-  <div key="payment-history" title="Payment history">
-    <p>We are showing customer payment history here</p>
-  </div>
-  <div key="payment-methods" title="Payment methods">
-    <p>We are showing customer payment methods here</p>
-  </div>
-</Tabs>
-)
-}
+      <div key="info" title="Customer info">
+        <p>We are showing customer info here</p>
+      </div>
+      <div key="payment-history" title="Payment history">
+        <p>We are showing customer payment history here</p>
+      </div>
+      <div key="payment-methods" title="Payment methods">
+        <p>We are showing customer payment methods here</p>
+      </div>
+    </Tabs>
+  );
+};
 
-export default Pooja;   
+export default Pooja;
